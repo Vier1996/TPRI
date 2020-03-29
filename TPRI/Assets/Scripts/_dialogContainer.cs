d5520f6b6709c9aa21c3f884b1f299dcedda97aa
NEMEZ1Z <50182983+NEMEZ1Z@users.noreply.github.com>
2020-03-29 23:57:55 +0300

    3d Model

--- TPRI/Assets/Scripts/_dialogContainer.cs
+++ TPRI/Assets/Scripts/_dialogContainer.cs
@@ -10,10 +10,34 @@ public class _dialogContainer : MonoBehaviour
     protected string[] VisitDialogs = new string[30];
     void Awake()
     {
+       BaseDiaolgsInitial();
+       WhereDiaolgsInitial();
+       ProblemsDiaolgsInitial();
+       VisitDiaolgsInitial();
+    }
+    private void BaseDiaolgsInitial() {
         BaseDialogs[0] = "Привет. Меня зовут Саня, я в порядке!";
+        BaseDialogs[1] = "...";
+        BaseDialogs[2] = "Здравствуйте! ";
+        
+    }
+    private void WhereDiaolgsInitial() {
         WhereDialogs[0] = "С пушкарей.";
+        WhereDialogs[1] = "...";
+        WhereDialogs[2] = "С шираги.";
+       
+    }
+    private void ProblemsDiaolgsInitial() {
         ProblemsDialogs[0] = "Временами не в порядке.";
+        ProblemsDialogs[1] = "...";
+        ProblemsDialogs[2] = "";
+        
+    }
+    private void VisitDiaolgsInitial() {
         VisitDialogs[0] = "Ну это... да, ну ты понял.";
+        VisitDialogs[1] = "...";
+        VisitDialogs[2] = "";
+        
     }
 
   
