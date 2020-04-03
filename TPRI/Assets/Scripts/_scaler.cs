527f50e3bb1baf22196c2bfc8369cabd6fddc104
Max <60153365+Vier1996@users.noreply.github.com>
2020-04-04 01:04:06 +0300

    asd

---
+++ TPRI/Assets/Scripts/_scaler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _scaler : MonoBehaviour
+{
+    [SerializeField] public float _Scale = 1;
+    void Start()
+    {
+        transform.localScale = new Vector3(_Scale, _Scale, _Scale);
+    }
+    
+}
