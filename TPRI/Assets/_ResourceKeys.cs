bc51f30afc7ecb50e51dbf2a3960b8b056d1936a
Max <60153365+Vier1996@users.noreply.github.com>
2020-04-10 02:28:32 +0300

    SkillMenu

--- TPRI/Assets/_ResourceKeys.cs
+++ TPRI/Assets/_ResourceKeys.cs
@@ -9,23 +9,35 @@ public class _ResourceKeys : MonoBehaviour
     public const string CharacterLevel = "CharacterLevel";
     public const string Town_HealthPeople = "Town_HealthPeople";
 
-    private const string R1 = "";
-    private const string R2 = "";
-    private const string R3 = "";
-    private const string R4 = "";
-    private const string R5 = "";
+    public const string Defense_1 = "Key_Defense_1";
+    public const string Defense_2 = "Key_Defense_2";
+    public const string Healing_1 = "Key_Healing_1";
+    public const string Healing_2 = "Key_Healing_2";
+    public const string Detection = "Key_Detection";
+    public const string Researcher = "Key_Researcher";
+    public const string Warning_1 = "Key_Warning_1";
+    public const string Warning_2 = "Key_Warning_1";
+    public const string SecondShoot_1 = "Key_SecondShoot_1";
+    public const string SecondShoot_2 = "KeySecondShoot_2";
+    public const string SecondShoot_3 = "Key_SecondShoot_3";
+    public const string LifeCity_1 = "Key_LifeCity_1";
+    public const string LifeCity_2 = "Key_LifeCity_2";
+    public const string SupremeEye = "Key_SupremeEye";
+    public const string FullHealing = "Key_FullHealing";
     
-    private const string R6 = "";
-    private const string R7 = "";
-    private const string R8 = "";
-    private const string R9 = "";
-    private const string R10 = "";
+    public const string Mixture = "Key_Mixture";
+    public const string Tincture = "Key_Tincture";
+    public const string Healing_salve = "Key_Healing_salve";
+    public const string Candy = "Key_Candy";
+    public const string Panacea = "Key_Panacea";
     
-    private const string R11 = "";
-    private const string R12 = "";
-    private const string R13 = "";
-    private const string R14 = "";
-    private const string R15 = "";
+    public const string Bunch_of_rosemary = "Key_Bunch_of_rosemary";
+    public const string Orchid_Leaf = "Key_Orchid_Leaf";
+    public const string Gargoyle_saliva = "Key_Gargoyle_saliva";
+    public const string Ginger_root = "Key_Ginger_root";
+    public const string Liver_eel = "Key_Mixture";
+    
+    
     
     private void Awake()
     {
