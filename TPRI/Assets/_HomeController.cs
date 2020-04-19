1006ae387f3330c927805e4ce1248dd120841673
Max <60153365+Vier1996@users.noreply.github.com>
2020-04-20 02:30:46 +0300

    SkillsPoints_SkillScene_Achivements_Equipments_Defense_2

--- TPRI/Assets/_HomeController.cs
+++ TPRI/Assets/_HomeController.cs
@@ -10,7 +10,7 @@ public class _HomeController : MonoBehaviour
    [SerializeField] private Button _store;
    [SerializeField] private Button _skills;
    [SerializeField] private Button _eqiepments;
-   [SerializeField] private Button _statistic;
+   [SerializeField] private Button _achievements;
    
    [SerializeField] private Button _back;
 
@@ -19,6 +19,7 @@ public class _HomeController : MonoBehaviour
       _store.onClick.AddListener(() => { SceneManager.LoadScene("_store");});
       _skills.onClick.AddListener(() => { SceneManager.LoadScene("_skills");});
       _eqiepments.onClick.AddListener(() => { SceneManager.LoadScene("DiscoverMenu");});
+      _achievements.onClick.AddListener(() => { SceneManager.LoadScene("_achivements"); });
       _back.onClick.AddListener(() => { SceneManager.LoadScene("_introMenu"); });
    }
 }
